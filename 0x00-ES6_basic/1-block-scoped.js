38ff9c223f32a3c3cff98e74cfe616f9a2fe8f7e
Jojo <jojothomas1515@gmail.com>
2023-07-24 12:19:18 +0100

    changed function var to const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,9 @@
 export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    //
   }
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   return [task, task2];
 }
