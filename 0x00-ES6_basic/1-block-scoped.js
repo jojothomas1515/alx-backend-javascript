9b558008dcceac6a934c452d9187dd213658c798
Jojo <jojothomas1515@gmail.com>
2023-07-24 12:14:47 +0100

    removed docs

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    let task = true;
+    let task2 = false;
   }
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   return [task, task2];
 }
