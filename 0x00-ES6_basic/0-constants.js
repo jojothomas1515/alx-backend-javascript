57eb70d15f3c39094803fefe0632f6ebb341ca0c
Jojo <jojothomas1515@gmail.com>
2023-07-24 12:12:19 +0100

    removed docs

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,30 +1,12 @@
-/**
- * # taskFirst
- *
- * @description: "makes a constant string"
- * ## Returns
- * @returns: a string
- */
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-/**
- * # getLast
- * @returns: "is okay"
- */
 export function getLast() {
   return ' is okay';
 }
 
-/**
- * # taskNext
- * ## Description
- * creates a string with let and concatenate to it with
- * the return value from ```getLast()```
- * @returns the concatenated value.
- */
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
