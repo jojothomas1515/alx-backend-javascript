f657ca6c40b047772d6ce5e396e76933a7caad1e
Jojo <jojothomas1515@gmail.com>
2023-07-26 17:49:53 +0100

    asynchronous function with try and catch example

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,8 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   try {
     return {
-      photo: await uploadPhoto,
-      user: await createUser,
+      photo: await uploadPhoto(),
+      user: await createUser(),
     };
   } catch (err) {
     return {
