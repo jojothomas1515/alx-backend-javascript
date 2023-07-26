fe115343ca733849f0ddae5e8e79857077f3169c
Jojo <jojothomas1515@gmail.com>
2023-07-26 23:56:48 +0100

    changed task 6 to use Promise.settled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const prom = Promise.resolve();
-  return prom.then(() => signUpUser(firstName, lastName).then((data) => ({ status: 'fulfilled', value: data })))
-    .then((data) => uploadPhoto(fileName).catch((err) => [data, { status: 'rejected', value: err }]));
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promises)
+    .then((results) => results.map((result) => result));
 }
