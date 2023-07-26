46e10c58c14f9045f5105f5b1e4e997844521ec8
Jojo <jojothomas1515@gmail.com>
2023-07-26 11:00:52 +0100

    create user and upload photo

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,6 +4,6 @@ export default function handleProfileSignup() {
   return createUser().then((user) => {
     uploadPhoto().then((pic) => {
       console.log(`${pic.body} ${user.firstName} ${user.lastName}`);
-    });
+    }).catch(() => console.log('Signup system offline'));
   }).catch(() => console.log('Signup system offline'));
 }
