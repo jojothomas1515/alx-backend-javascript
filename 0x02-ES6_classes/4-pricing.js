d5c8a28c747845145da4f539e8e280d457941386
Jojo <jojothomas1515@gmail.com>
2023-07-27 19:07:59 +0100

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw TypeError('currency must be and instance of Currency');
+    }
+    this._currency = currency;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
