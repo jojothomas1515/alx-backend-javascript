91b87f3f20aeab58710a1152520766f7b78ec897
Jojo <jojothomas1515@gmail.com>
2023-08-01 17:29:26 +0100

    typedint8array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   const Buf = new ArrayBuffer(length);
   const i8view = new Int8Array(Buf);
-  if (i8view.length < position) {
+  if (i8view.length - 1 < position) {
     throw Error('Position outside range');
   }
   i8view[position] = value;
