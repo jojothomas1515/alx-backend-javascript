1d327d46919e520018d494dc95fc75582b60c67b
Jojo <jojothomas1515@gmail.com>
2023-08-01 20:50:23 +0100

    grocerieslist

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const mm = new Map();
+  mm.set('Apples', 10);
+  mm.set('Tomatoes', 10);
+  mm.set('Pasta', 1);
+  mm.set('Rice', 1);
+  mm.set('Banana', 5);
+  return mm;
+}
