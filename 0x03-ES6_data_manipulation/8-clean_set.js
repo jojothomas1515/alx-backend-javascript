0424c04560ac480ff8e6b20f5663246c0c6fe780
Jojo <jojothomas1515@gmail.com>
2023-08-02 10:43:29 +0100

    return an empty string when word it not a sstring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, word = '') {
-  if (set instanceof Set && word !== '' && word instanceof String) {
+  if (set instanceof Set && word !== '' && typeof word === 'string') {
     return Array.from(set).filter((w) => w.startsWith(word) && (w.slice(word.length) !== ''))
       .map((w) => w.substring(word.length))
       .join('-');
