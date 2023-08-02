6bb49ab755134ead543950a111ee7bd20bdb8646
Jojo <jojothomas1515@gmail.com>
2023-08-02 10:46:19 +0100

    return an empty string when word it not a sstring

--- 0x03-ES6_data_manipulation/main/8-main.js
+++ 0x03-ES6_data_manipulation/main/8-main.js
@@ -2,3 +2,5 @@ import cleanSet from '../8-clean_set';
 
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana', 'bon']), 'bon'));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+const set = new Set(['id-test', 'id-chicken', 'id-user', , 'id-id-']);
+console.log(cleanSet(set, 'id-'));
