31f443b71c9d5071075b06fb93dfeb8eae6513b4
Jojo Thomas <jojothomas1515@gmail.com>
2023-09-20 00:53:02 +0100

    http setup

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+});
+
+const port = process.env.PORT || 5000;
+app.listen(1245, () => console.log(`listening on port ${port}`));
+
+module.exports = app;
