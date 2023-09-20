bb2f559c0e0e3bec7442b48a4e90bbc3580b2263
Jojo Thomas <jojothomas1515@gmail.com>
2023-09-20 01:04:45 +0100

    readfile promises version

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,43 +1,40 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
+const { readFile } = require('fs/promises');
 
-async function countStudent(fileName) {
-  return new Promise((resolve, reject) => {
-    let count = 0;
+async function countStudents(fileName) {
+  try {
     const dept = new Map();
-    fs.readFile(fileName, 'utf8', (err, data) => {
-      if (err) {
-        reject(Error('Cannot load the database'));
-      }
-      const lines = data.trim().split('\n');
-      const students = lines.slice(1);
-      students.forEach((student) => {
-        if (student !== '') {
-          const sp = student.split(',');
-          const fn = sp[0];
-          const ln = sp[1];
-          const age = sp[2];
-          const field = sp[3];
-          if (fn && ln && age && field) {
-            count += 1;
+    let count = 0;
+    const data = await readFile(fileName, 'utf8');
+    const lines = data.trim().split('\n');
+    const students = lines.slice(1);
+    students.forEach((student) => {
+      if (student !== '') {
+        const sp = student.split(',');
+        const fn = sp[0];
+        const ln = sp[1];
+        const age = sp[2];
+        const field = sp[3];
+        if (fn && ln && age && field) {
+          count += 1;
 
-            if (!dept.has(field)) {
-              dept.set(field, [fn]);
-            } else {
-              const li = dept.get(field);
-              li.push(fn);
-            }
+          if (!dept.has(field)) {
+            dept.set(field, [fn]);
+          } else {
+            const li = dept.get(field);
+            li.push(fn);
           }
         }
-      });
-      console.log(`Number of students: ${count}`);
-      dept.forEach((v, k) => {
-        console.log(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
-      });
+      }
     });
-    resolve();
-  });
+    console.log(`Number of students: ${count}`);
+    dept.forEach((v, k) => {
+      console.log(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
+    });
+  } catch (err) {
+    throw Error('Cannot load the database');
+  }
 }
 
-module.exports = countStudent;
+module.exports = countStudents;
