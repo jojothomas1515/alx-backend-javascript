ef061a00c228ec835c1e3af683f9a196fc583565
Jojo Thomas <jojothomas1515@gmail.com>
2023-09-21 06:46:04 +0100

    Http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const http = require('http');
-const countStudents = require('./3-read_file_async');
 
 const { argv } = process;
 let db = '';
@@ -9,20 +8,60 @@ if (argv.length > 2) {
   // eslint-disable-next-line prefer-destructuring
   db = argv[2];
 }
-const app = http.createServer((req, res) => {
+
+const { readFile } = require('fs');
+const { promisify } = require('util');
+
+async function countStudents(fileName, res) {
+  try {
+    const dept = new Map();
+    let count = 0;
+    const rf = promisify(readFile);
+    const data = await rf(fileName, 'utf8');
+    const lines = data.trim().split('\n');
+    const students = lines.slice(1);
+    students.forEach((student) => {
+      if (student !== '') {
+        const sp = student.split(',');
+        const fn = sp[0];
+        const ln = sp[1];
+        const age = sp[2];
+        const field = sp[3];
+        if (fn && ln && age && field) {
+          count += 1;
+
+          if (!dept.has(field)) {
+            dept.set(field, [fn]);
+          } else {
+            const li = dept.get(field);
+            li.push(fn);
+          }
+        }
+      }
+    });
+    res.write('This is the list of our students');
+
+    res.write(`Number of students: ${count}\n`);
+    dept.forEach((v, k) => {
+      res.write(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}\n`);
+    });
+  } catch (err) {
+    throw Error('Cannot load the database');
+  }
+}
+
+const app = http.createServer(async (req, res) => {
   res.setHeader('Content-Type', 'text/html');
   if (req.url === '/students') {
-    res.write('This is the list of our students');
     try {
-      res.write(countStudents(db));
+      await countStudents(db, res);
     } catch (err) {
       res.write(err.message);
     }
-    res.end();
+    return res.end();
   }
-  res.setHeader('Content-Type', 'text/html');
   res.write('Hello Holberton School!');
-  res.end();
+  return res.end();
 });
 
 const port = process.env.PORT || 1245;
