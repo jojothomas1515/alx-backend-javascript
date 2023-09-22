6e23094aea5686edcd848a3730137452c17098ad
Jojo Thomas <jojothomas1515@gmail.com>
2023-09-22 17:38:37 +0100

    calculateNumber function test with chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -8,10 +8,14 @@ describe('test suite for calculate number function', () => {
   it('add two floating point numbers', () => {
     expect(calculateNumber('SUM', 10.3, 5.2)).to.equal(15);
   });
-  it('subtract two floating point numbers', () => {
-    expect(calculateNumber('SUBTRACT', 10.3, 5.2)).to.equal(5);
-  });
+
   it('divide with 0', () => {
     expect(calculateNumber('DIVIDE', 10.3, 0)).equal('Error');
+  });
+});
+
+describe('subtract test suite', () => {
+  it('subtract two floating point numbers', () => {
+    expect(calculateNumber('SUBTRACT', 10.3, 5.2)).to.equal(5);
   });
 });
