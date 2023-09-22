ca20787aa4fc909243e8234063cbc83a2fe1aa25
Jojo Thomas <jojothomas1515@gmail.com>
2023-09-22 15:34:55 +0100

    calculateNumber function test with chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,19 +1,18 @@
 const assert = require('assert');
-const calculateNumber = require('./1-calcul');
 const { expect } = require('chai');
-const exp = require('constants');
+const calculateNumber = require('./1-calcul');
 
 describe('test suite for calculate number function', () => {
   it('divide two floating point numbers', () => {
-    expect(calculateNumber('DIVIDE', 10.3, 5.2)).equal(2);
+    expect(calculateNumber('DIVIDE', 10.3, 5.2)).to.equal(2);
   });
   it('add two floating point numbers', () => {
-    expect(calculateNumber('SUM', 10.3, 5.2)).equal(15);
+    expect(calculateNumber('SUM', 10.3, 5.2)).to.equal(15);
   });
   it('subtract two floating point numbers', () => {
-    expect(calculateNumber('SUBTRACT', 10.3, 5.2)).equal(5);
+    expect(calculateNumber('SUBTRACT', 10.3, 5.2)).to.equal(5);
   });
   it('divide with 0', () => {
-    expect(calculateNumber('DIVIDE', 10.3, 0)).equal('Error');
+    expect(calculateNumber('DIVIDE', 10.3, 0)).to.equal('Error');
   });
 });
