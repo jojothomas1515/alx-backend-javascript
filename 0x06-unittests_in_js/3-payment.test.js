878e8bf38ad9ca2850fdeac9fff0ff5790343ff7
Jojo Thomas <jojothomas1515@gmail.com>
2023-09-23 00:35:46 +0100

    3 payment

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,12 +1,17 @@
 const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
+const Util = require('./utils');
 const sinon = require('sinon');
 
 describe('using sinon spy', () => {
   const spy = sinon.spy(sendPaymentRequestToApi);
+  const calMethod = sinon.spy(Util, 'calculateNumber');
+  spy(202, 8);
 
   it('check of sendPaymentRequestToApi work', () => {
-    spy(202, 8);
     expect(spy.calledOnceWith(202, 8)).equal(true);
+  });
+  it('check wether calculateNumber method was called', () => {
+    expect(calMethod.calledOnce).equal(true);
   });
 });
