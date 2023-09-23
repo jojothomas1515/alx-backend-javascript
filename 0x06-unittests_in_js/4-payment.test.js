79094d38498a7a38ecf01631f95b602b3e673f3b
Jojo Thomas <jojothomas1515@gmail.com>
2023-09-23 01:02:42 +0100

    4 payment

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,16 +4,19 @@ const Util = require('./utils');
 const sinon = require('sinon');
 
 describe('using sinon spy', () => {
-  const spy = sinon.spy(sendPaymentRequestToApi);
-  const calMethod = sinon
-    .stub(Util, 'calculateNumber')
-    .callsFake((a, b) => a + b);
-  spy(202, 8);
+  // const spy = sinon.spy(sendPaymentRequestToApi);
+  // eslint-disable-next-line no-unused-vars
+  const calMethod = sinon.stub(Util, 'calculateNumber').callsFake((a, b) => 10);
+  sendPaymentRequestToApi(22, 88);
+  const log = sinon.spy(console, 'log');
 
-  it('check of sendPaymentRequestToApi work', () => {
-    expect(spy.calledOnceWith(202, 8)).equal(true);
-  });
+  // it('check of sendPaymentRequestToApi work', () => {
+  //   expect(spy.calledOnceWith(202, 8)).equal(true);
+  // });
   it('check wether calculateNumber method was called', () => {
     expect(calMethod.calledOnce).equal(true);
+    // expect(log.calledOnce).equal(true);
   });
+  // it('test console log', () => {
+  // });
 });
