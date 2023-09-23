f1c5ecfc2d96307777184ba0ed9f24096aaf24e0
Jojo Thomas <jojothomas1515@gmail.com>
2023-09-23 18:28:08 +0100

    testing api route functionality

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,54 @@
+/* eslint-disable */
+const chai = require('chai');
+const request = require('request');
+const sinon = require('sinon');
+
+describe('Test suite for index page', () => {
+  it('test status code', (done) => {
+    request.get('http://localhost:7865', (err, res) => {
+      if (err) {
+        done(err);
+      }
+      chai.expect(res.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+  it('test response body', (done) => {
+    request.get('http://localhost:7865', (err, res) => {
+      if (err) {
+        done(err);
+      }
+      chai.expect(res.body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+describe('Test suite for getting cart', () => {
+  it('test status code', (done) => {
+    request.get('http://localhost:7865/cart/22', (err, res) => {
+      if (err) {
+        done(err);
+      }
+      chai.expect(res.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+  it('test response body', (done) => {
+    request.get('http://localhost:7865/cart/22', (err, res) => {
+      if (err) {
+        done(err);
+      }
+      chai.expect(res.body).to.be.equal('Payment methods for cart 22');
+      done();
+    });
+  });
+  it('test passing non number id', (done) => {
+    request.get('http://localhost:7865/cart/jojo', (err, res) => {
+      if (err) {
+        done(err);
+      }
+      chai.expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+});
